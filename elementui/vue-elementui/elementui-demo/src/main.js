55241fb5be60a81426c427512cba331a2faa2b4f
lijing <lijing@bjdvt.com>
2018-12-06 14:28:32 +0800

    babel-polyfill

--- elementui/vue-elementui/elementui-demo/src/main.js
+++ elementui/vue-elementui/elementui-demo/src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import "babel-polyfill"; // 解决Ie9
+
 import Vue from 'vue'
 import 'normalize.css/normalize.css' //css reset
 import App from './App'
